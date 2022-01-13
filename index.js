469d690e22b56b16a988543e45a667f8d7f7dde3
rahman-a <ahm.abdrahman@gmail.com>
2022-01-13 22:30:33 +0200

    fix the server port

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
 
-app.listen('5000', () => {
+const port = process.env.PORT || 5000
+app.listen(port, () => {
     console.log('port is running at 5000');
 })
