81884f9f0c738d7b5b93959db9e8bfc4725d1949
rahman-a <ahm.abdrahman@gmail.com>
2022-01-15 12:23:40 +0200

    adjust the user image size in navbar on small screens

--- client/src/icons/Lock.js
+++ client/src/icons/Lock.js
@@ -7,7 +7,7 @@ const Lock = (props) => {
         focusable="false" 
         data-prefix="fas" 
         data-icon="lock" 
-        class="svg-inline--fa fa-lock fa-w-14" 
+        className="svg-inline--fa fa-lock fa-w-14" 
         role="img" 
         xmlns="http://www.w3.org/2000/svg" 
         viewBox="0 0 448 512"
